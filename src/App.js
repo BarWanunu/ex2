1e1a947e7245fe3ab5dec28c15e3b7130ae5954b
shakedg1234 <shaked.gitta@gmail.com>
2024-01-31 22:58:22 +0200

    adding like option

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Post from './Post/post.js';
-import posts from './data/postdb.json'
-import { useState } from 'react';
+
 
 function App() {
   return (
-        posts.map((post)=>
-        <Post {...post}/>
-        )
+    <Post
+      id="1"
+      text="hi"
+      profile="shaked"
+      date="20.1.24"
+    ></Post>
   )
 
 }
--- src/Post/post.js
+++ src/Post/post.js
@@ -1,18 +1,47 @@
 import './post.css';
-//import { ReactComponent as Heart } from './heart2.svg';
+import { ReactComponent as Like } from './svgimg/like.svg';
+import { ReactComponent as Share } from './svgimg/share.svg';
+import { ReactComponent as Comment } from './svgimg/comment.svg';
+import React, { useState } from 'react';
 
-function Post({ id, text,profile , date, img }) {
+const LikeButton = () => {
+    const [liked, setLiked] = useState(false);
+    const [likes, setLikes] = useState(0);
+  
+    const handleLikeClick = () => {
+      if (liked) {
+        setLikes((prevLikes) => prevLikes - 1);
+      } else {
+        setLikes((prevLikes) => prevLikes + 1);
+      }
+      setLiked(!liked);
+    };
+
   return (
-    <a href={`post.html?id=${id}`}>
-      <post>
-        <h3>{text}</h3>
-        <span>
-          {profile}
-          <time>{date}</time>
-        </span>
-        
-      </post>
-    </a>
+    <>
+      <svg width="25" height="25" onClick={handleLikeClick}>
+        <Like />
+      </svg>
+      <span>{likes} </span>
+    </>
+  );
+};
+
+function Post({ id, text, profile, date, img }) {
+  return (
+    <article className='postdesign'>
+      <span>
+        <b>{profile}&nbsp;</b>
+        <br/>
+        <time>{date}</time>
+      </span>
+      <p>{text}</p>
+      <span>
+        <LikeButton />
+        <Share width="25" height="25"></Share>
+        <Comment width="25" height="25"></Comment>
+      </span>
+    </article>
   );
 }
 
