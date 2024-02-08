2f56db29a5231df33a9d7614d9d3e86db48646bb
Itay Chananel <itay17283@gmail.com>
2024-02-08 10:37:45 +0200

    commit home-page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 
+import Home from './Home_page/home.js';
 import Post from './Post/post.js';
 
 
 function App() {
   return (
-    <Post
-      id="1"
-      text="hi"
-      profile="shaked"
-      date="20.1.24"
-    ></Post>
+    <div>
+      <Home>
+      </Home>
+      <Post
+        id="1"
+        text="hi"
+        profile="shaked"
+        date="20.1.24"
+      ></Post>
+    </div>
   )
 
 }
