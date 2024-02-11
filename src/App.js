358d451780f2426f45ceff89b3c99dc55c8d09de
BarWanunu <barwanunu6@gmail.com>
2024-02-11 22:29:53 +0200

    some updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
 import Home from './Home_page/home.js';
 import Post from './Post/post.js';
-
-
+import Signup from './signup_page/signup.js';
+import './signup_page/signup.css';
 function App() {
   return (
     <div>
-      <Home>
-      </Home>
+      <Signup>
+      </Signup>
     </div>
   )
 
--- src/signup_page/signup.js
+++ src/signup_page/signup.js
@@ -31,48 +31,92 @@ function Signup() {
         alert('Form submitted successfully!');
     };
     return(
-    <div>
-        <div>
+        <>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Home Page</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+        <title>Sign in page</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+          crossOrigin="anonymous"
+        />
         <link rel="stylesheet" href="signup.css" />
-      </div>
-        <div className="container px-6 text-center form-container">
-        <div className="row align-items-center">
-            <div className="col">
-                <div id="Email Address" className="form-text mb-3">
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email address" />
-                    <label htmlFor="exampleFormControlInput1" className="form-label"></label>
-                    <small>Your email address needs to be in the form of: name@example.com</small>
-                </div>
-                <div id="passwordHelpBlock" className="form-text mb-3">
-                    <input type="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" placeholder="Password" />
-                    <label htmlFor="inputPassword5" className="form-label"></label>
-                    <small>Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.</small>
-                </div>
-                <div id="passwordVerification" className="form-text mb-3">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Confirm Password" />
-                    <label htmlFor="passwordVerification" className="form-label"></label>
-                </div>
-                <div id="displayName" className="form-text mb-3">
-                    <input type="text" className="form-control" id="displayName" placeholder="Display Name" />
-                    <label htmlFor="displayName" className="form-label"></label>
-                </div>
-
-                <div className="photo-upload mb-3">
-                    <input type="file" className="form-control" id="photoInput" accept="image/*" />
-                    <label htmlFor="photoInput" className="form-label">Choose Your Profile Picture</label>
-                </div>
-                <div className="col-auto">
-                <button type="button" className="btn btn-primary mb-3" style={{ padding: '5px' }} onClick={handleSubmit}>Submit</button>
-                </div>
-            </div>
+        <div className="col-4">
+          <img src="facebook.svg" width={1525} height={200} />
         </div>
-    </div>
-    </div>
+        <div className="container px-6 text-center form-container">
+          <div className="row align-items-center">
+            <div className="col">
+              <div id="Email Address" className="form-text-mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleFormControlInput1"
+                  placeholder="Email address"
+                />
+                <label htmlFor="exampleFormControlInput1" className="form-label" />
+                <small>
+                  Your email address needs to be in the form of : name@example.com
+                </small>
+              </div>
+              <div id="passwordHelpBlock" className="form-text mb-3">
+                <input
+                  type="password"
+                  id="inputPassword5"
+                  className="form-control"
+                  aria-describedby="passwordHelpBlock"
+                  placeholder="Password"
+                />
+                <label htmlFor="inputPassword5" className="form-label" />
+                <small>
+                  Your password must be 8-20 characters long, contain letters and
+                  numbers, and must not contain spaces, special characters, or emoji.
+                </small>
+              </div>
+              <div id="passwordVerification" className="form-text mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="floatingPassword"
+                  placeholder="Confirm Password"
+                />
+                <label htmlFor="passwordVerification" className="form-label" />
+              </div>
+              <div id="displayName" className="form-text mb-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="displayName"
+                  placeholder="Display Name"
+                />
+                <label htmlFor="displayName" className="form-label" />
+              </div>
+              <div className="photo-upload mb-3">
+                <input
+                  type="file"
+                  className="form-control"
+                  id="photoInput"
+                  accept="image/*"
+                />
+                <label htmlFor="photoInput" className="form-label">
+                  Choose Your Profile Picture
+                </label>
+              </div>
+              <div className="col-auto">
+                <button
+                  type="submit"
+                  className="btn btn-primary mb-3"
+                  style={{ padding: 5 }}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+      
 );
 }
 
