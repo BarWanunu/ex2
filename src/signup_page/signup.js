c16882349c1d88ff42b5e26d6ae1a46a31310a60
BarWanunu <barwanunu6@gmail.com>
2024-02-12 01:26:36 +0200

    updates

--- src/signup_page/signup.js
+++ src/signup_page/signup.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './signup.css';
+import facebook from './facebook.svg';
 
 function Signup() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const toggleDarkMode = () => {
+            setIsDarkMode((prevMode) => !prevMode);
+    };
+        useEffect(() => {
+            document.body.classList.toggle('dark-mode', isDarkMode);
+          }, [isDarkMode]);
+          
+        
     const handleSubmit = () => {
         const emailInput = document.getElementById('exampleFormControlInput1');
         const passwordInput = document.getElementById('inputPassword5');
         const confirmPasswordInput = document.getElementById('floatingPassword');
+        const nameInput = document.getElementById('displayName');
+        const photoInput = document.getElementById('photoInput');
 
         // Check if email is in the correct format
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -27,6 +39,16 @@ function Signup() {
             return;
         }
 
+        if(nameInput.value == '') {
+        alert('Please enter a valid name');
+        return;
+        }
+
+        if(photoInput.value == '') {
+        alert('Please upload a profile picture');
+        return;
+        }
+
         // Perform additional actions or submit the form if everything is valid
         alert('Form submitted successfully!');
     };
@@ -35,14 +57,24 @@ function Signup() {
         <div>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Home Page</title>
+        <title>Sign up Page</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
         <link rel="stylesheet" href="signup.css" />
+        <div className="col-4">
+    <img src={facebook} width={1400} height={150}/>
+    <button type="button" id="Light" className={`btn btn-light ${isDarkMode ? 'active' : ''}`} onClick={toggleDarkMode}>
+            Light
+          </button>
+          <button type="button" id="Dark" className={`btn btn-dark ${isDarkMode ? '' : 'active'}`} onClick={toggleDarkMode}>
+            Dark
+          </button>
+  </div>
       </div>
         <div className="container px-6 text-center form-container">
         <div className="row align-items-center">
-            <div className="col">
+            <div className="col middle">
+                <div className='text-center'>
                 <div id="Email Address" className="form-text mb-3">
                     <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email address" />
                     <label htmlFor="exampleFormControlInput1" className="form-label"></label>
@@ -58,16 +90,16 @@ function Signup() {
                     <label htmlFor="passwordVerification" className="form-label"></label>
                 </div>
                 <div id="displayName" className="form-text mb-3">
-                    <input type="text" className="form-control" id="displayName" placeholder="Display Name" />
+                <input type="text" className="form-control" id="displayName" placeholder="Display Name" required/>
                     <label htmlFor="displayName" className="form-label"></label>
                 </div>
-
                 <div className="photo-upload mb-3">
                     <input type="file" className="form-control" id="photoInput" accept="image/*" />
                     <label htmlFor="photoInput" className="form-label">Choose Your Profile Picture</label>
                 </div>
                 <div className="col-auto">
                 <button type="button" className="btn btn-primary mb-3" style={{ padding: '5px' }} onClick={handleSubmit}>Submit</button>
+                </div>
                 </div>
             </div>
         </div>
